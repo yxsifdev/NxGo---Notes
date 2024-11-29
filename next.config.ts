eaaf8a74709bbb46b254ed05e903a97064606743
yxsifdev <yosifblancotafur2006@gmail.com>
2024-11-29 17:03:58 -0500

    Update Next Config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "avatars.githubusercontent.com" }],
   },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+  },
 };
 
 export default nextConfig;
